622b694886d6ae3ae7c53dea3627c5f865d66014
João Daniel da Purificação dos Santos <joaodaniel.purificacao@gmail.com>
2021-05-21 11:17:30 -0300

    Site Estilização Finalizada

--- Projeto_Final/Documentacao/send_mail.php
+++ Projeto_Final/Documentacao/send_mail.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./meu.css">
+    <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" a href="./resources/bootstrap.css">
     <title>Pizzaria</title>
     <link rel="shortcut icon" type="image/jpg" href="./images/logo.png"/>
