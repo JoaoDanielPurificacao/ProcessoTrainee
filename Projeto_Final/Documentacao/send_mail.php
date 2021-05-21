6c42c8fde6fb0468531809104253557956b0a62b
João Daniel da Purificação dos Santos <joaodaniel.purificacao@gmail.com>
2021-05-21 11:06:57 -0300

    Botão Top

--- Projeto_Final/Documentacao/send_mail.php
+++ Projeto_Final/Documentacao/send_mail.php
@@ -263,6 +263,8 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d971.9465258528716!2d-38.515574743912545!3d-12.985528353351796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDU5JzA3LjciUyAzOMKwMzAnNTUuNyJX!5e0!3m2!1spt-BR!2sbr!4v1621271328710!5m2!1spt-BR!2sbr" width="" height="" style="border:0;" allowfullscreen="" loading="lazy" class="map"></iframe>
     </section>
 
+    <a href="#home" class="top"></a>
+
 
 </body>
 </html>
